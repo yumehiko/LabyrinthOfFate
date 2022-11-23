db8999e5b025d660cc215ddd24841042181b1ab5
yumehiko <46164249+yumehiko@users.noreply.github.com>
2022-11-23 19:26:32 +0900

    Project Import

---
+++ Assets/Scripts/Test/HelloWorldService.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace yumehiko
+{
+	public class HelloWorldService
+	{
+		private int count = 0;
+
+		public void Hello(string message = "Hello world")
+		{
+			UnityEngine.Debug.Log(message + count);
+			count++;
+		}
+	}
+}
